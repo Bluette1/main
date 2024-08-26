dd7bb4685ee246454f4f760d19269fd5030e8de0
Bluette1 <marylene.sawyer@gmail.com>
2024-08-26 16:43:07 +0200

    Add Routes - main

--- main.py
+++ main.py
@@ -1,20 +1,26 @@
-from flask import Flask
+from dataclasses import dataclass
+from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 from sqlalchemy import UniqueConstraint
+import requests
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root@db/main'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root@main-db-1/main'
 CORS(app)
 
 db = SQLAlchemy(app)
 
-
+@dataclass
 class Product(db.Model):
+  id: int
+  title: str
+  image: str
   id = db.Column(db.Integer, primary_key=True, autoincrement=False)
   title = db.Column(db.String(200))
   image = db.Column(db.String(200) )
 
+@dataclass
 class Product_User(db.Model):
   id = db.Column(db.Integer, primary_key=True)
   user_id = db.Column(db.Integer)
@@ -22,9 +28,16 @@ class Product_User(db.Model):
   UniqueConstraint('user_id', 'product_id', name='user_product_unique')
 
 
-@app.route('/')
+@app.route('/api/products')
 def index():
-  return 'Hello'
+  return jsonify(Product.query.all())
+
+
+@app.route('/api/products/<int:id>/like', methods=['POST'])
+def like(id):
+  req = requests.get('http://admin-backend-1:8000/api/user')
+
+  return jsonify(req.json())
 
 
 if __name__ == '__main__':
